8a46f68d63cb122b56c000dde0d75834555d797d
Anubhaw-Singh <vkt94594@gmail.com>
2025-12-03 23:08:14 +0530

    updated bootstrap classes for TD form

--- MdAdil/src/app/app.component.ts
+++ MdAdil/src/app/app.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MyTemplateFormComponent } from './my-template-form/my-template-form.component';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MyTemplateFormComponent],
+  imports: [RouterOutlet,MyTemplateFormComponent,ReactiveFormsComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'LearningNeverStops';
+  isTemplate: boolean = false;
+  isReactive: boolean = false;
+  ngOnInit(){
+    this.isReactive = true;
+  }
+  showTemplateForm(){
+    this.isTemplate = true;
+    this.isReactive = false;
+  }
+  showReactiveForm(){
+    this.isReactive = true;
+    this.isTemplate = false;
+  }
 }
