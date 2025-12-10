c99787774fd9d3f2c000b9c7e9762d1d2a947d05
Anubhaw-Singh <vkt94594@gmail.com>
2025-12-10 07:36:09 +0530

    anubhaw/created required service,components and linked bootstrap in index.html

---
+++ Services/src/app/student/studentdata/studentdata.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-studentdata',
+  standalone: true,
+  imports: [],
+  templateUrl: './studentdata.component.html',
+  styleUrl: './studentdata.component.css'
+})
+export class StudentdataComponent {
+
+}
